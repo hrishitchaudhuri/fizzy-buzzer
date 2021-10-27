335bbfe49792840473b361052da946108c28d362
undead-carbon <43005490+undead-carbon@users.noreply.github.com>
2021-10-27 11:32:34 +0530

    Commit #06

--- fizz-buzz/README.md
+++ fizz-buzz/README.md
@@ -7,7 +7,8 @@ The one shot method to determine your worth, candidate #42069.
 I'm waaay to lazy to explain it so here ya go : [Nice Wikipedia link](https://en.wikipedia.org/wiki/Fizz_buzz#Programming).
 
 ## Usage
-* Execute it as `./z.out [number of fizz-buzz numbers to be printed]`
+* Create executable using `make` or using `nasm`(64 bit format) and linking it.
+* Execute it as `./z.out`. It will prompt you for the number of fizz buzz numbers to be printed.
 
 ## Notes
-* There will be edge cases I haven't taken care of (interviewer shakes head in disappointment) so if you find any and manage to break the program feel free mention it in issues. It will be attributed to you in the fix. Or if you're feeling particularly nice you could also submit a fix for that bug
+* There **will** be edge cases I haven't taken care of (interviewer shakes head in disappointment). If you find any and manage to break the program, feel free mention it in issues. It will be attributed to you in the fix. Or if you're feeling particularly nice you could also submit a fix for that bug
---
+++ fizz-buzz/fizzbuzz.asm
@@ -0,0 +1,149 @@
+section .data
+	prompt db "Enter your number: ",0
+	fizz db "Fizz",0
+	buzz db "Buzz",0
+	newline db 10,0
+	
+; This is all initialized to zero
+section .bss
+	buffer resb 16 ; to store input
+	outbuf resb 16 ; to store output before it can be written
+	number resb 4 ; to store n (num of fb numbers)
+	
+section .text
+	global _start
+
+_start:
+	mov rax, prompt
+	call _print
+	; get n
+	call _getInput
+	; conv n to int
+	mov rax, buffer
+	call _atoi
+	; loop n times
+	mov r15, [number]
+	cmp r15, 0
+	je _exit
+	; the actual number
+	mov r14, 1
+start_loop:
+	mov rax, r14
+	call _itoa
+	mov rax, buffer
+	call _print
+	mov rax, newline
+	call _print
+	inc r14
+	dec r15
+	cmp r15, 0
+	jne start_loop
+
+	jmp _exit
+
+; convert value in rax to string and store in buffer
+_itoa:
+	; modulus
+	mov r10, 10
+	; number of chars
+	mov r11, 0
+
+; place digits onto stack
+loop2_:
+	mov rdx, 0
+	idiv r10
+	push rdx
+	inc r11
+	cmp rax, 0
+	jne loop2_
+
+; addr of next write loc in buffer
+	mov r10, buffer
+; pop off chars and move to buffer
+loop3_:
+	pop r12
+	add r12, 48 ; convert to ascii repr
+	mov [r10], r12 ; write to buffer
+	inc r10
+	dec r11
+	cmp r11, 0
+	jne loop3_
+	ret
+
+; convert the string in rax to an int & store in number
+_atoi:
+	mov r11, rax
+	call _clrNum
+	; used to build up the integer
+	; rax is the only reg used by mul
+	xor rax, rax
+	; constant 10 because mul doesn't allow operand to be a value o.O
+	mov r13, 10
+loop1_:
+	xor r12, r12
+	mov r12b, [r11]
+	cmp r12b, 0
+	je loop1_end
+	cmp r12b, 10
+	je loop1_end
+	sub r12, 48 ; conversion from ascii
+	inc r11
+	mul r13
+	add rax, r12
+	jmp loop1_	
+loop1_end:
+	mov [number], rax
+	ret
+
+; clear the buffer by setting it to all zeros
+_clrBuf:
+	mov al, 0
+	mov rsi, buffer
+	mov rdi, buffer
+	add rdi, 16
+	stosb
+	ret
+
+; clear the number buffer by setting it to all zeros
+_clrNum:
+	mov al, 0
+	mov rsi, number
+	mov rdi, number
+	add rdi, 4
+	stosb
+	ret
+
+; get the number from stdin and store it in buffer
+_getInput:
+	call _clrBuf	
+	mov rax, 0
+	mov rdi, 0
+	mov rsi, buffer
+	syscall	
+	ret
+
+; print the text pointed to by rax. Delimited by 0
+_print:
+	push rax ; start of the string
+	mov r10, 0 ; string length
+loop_:
+	mov cl, [rax]
+	cmp cl, 0
+	je loop_end
+	inc rax
+	inc r10
+	jmp loop_
+loop_end:
+
+	mov rax, 1
+	mov rdi, 1
+	pop rsi
+	mov rdx, r10
+	syscall
+
+	ret
+
+_exit:
+	mov rax, 60
+	mov rdi, 0
+	syscall
